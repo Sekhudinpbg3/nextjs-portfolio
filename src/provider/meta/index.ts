d983d9bd3ef7e7973d0e61099f7a0969b2e07d01
SekhudinPbg3 <Sekhudinpbg3@gmail.com>
2023-10-04 20:56:52 +0700

    change web title and add scroll behavior

--- src/provider/meta/index.ts
+++ src/provider/meta/index.ts
@@ -3,9 +3,6 @@ import { Metadata } from "next";
 
 export class Meta {
   public static default: Metadata = {
-    title: {
-      template: "%s | Djorna",
-      default: _.upperFirst(_.trim(`Djorna | Apps`)),
-    },
+    title: _.upperFirst(_.trim(`Sekhudin's Portfolio`)),
   };
 }
